3c43d96da4711515bf4e4f487440e0d7d7165fb7
Juan Navas <navascruzf@gmail.com>
2020-02-24 11:53:02 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,129 @@
+const axios = require('axios');
+const $ = require('cheerio');
+const Twitter = require('twitter');
+const airtmUrl = 'https://rates.airtm.com';
+const dolarToday = 'https://s3.amazonaws.com/dolartoday/data.json';
+
+// OCR
+const ocrSpaceApi = require('ocr-space-api');
+
+const options = {
+    apikey: '78f7cf9a7088957',
+    language: 'spa',
+    imageFormat: 'image/jpg',
+    isOverlayRequired: true
+};
+
+const getAirtmRates = _ =>
+    new Promise(resolve => {
+        axios.get(airtmUrl).then(res => {
+            let buy = $('.rate--buy', res.data).html();
+            let general = $('.rate--general', res.data).html();
+            let sell = $('.rate--sell', res.data).html();
+
+            const data = {
+                buy,
+                general,
+                sell
+            };
+
+            resolve(data);
+        });
+    });
+
+const getDolarToday = _ =>
+    new Promise(resolve => {
+        axios.get(dolarToday).then(res => {
+            let usd = res.data.USD;
+            let eur = res.data.EUR;
+            let newUsd = {},
+                newEur = {};
+            for (let key in usd) {
+                let newKey = key.replace('_', ' ');
+                newKey = newKey.charAt(0).toUpperCase() + newKey.slice(1);
+                newUsd[newKey] = usd[key];
+            }
+
+            for (let key in eur) {
+                let newKey = key.replace('_', ' ');
+                newKey = newKey.charAt(0).toUpperCase() + newKey.slice(1);
+                newEur[newKey] = eur[key];
+            }
+
+            resolve({ USD: newUsd, EUR: newEur });
+        });
+    });
+
+const getMonitor = _ =>
+    new Promise(resolve => {
+        const client = new Twitter({
+            consumer_key: process.env.CONSUMER_KEY,
+            consumer_secret: process.env.CONSUMER_SECRET,
+            access_token_key: process.env.ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        });
+
+        const params = { screen_name: 'monitordolarvzl' };
+        client.get('statuses/user_timeline', params, async function(
+            error,
+            tweets,
+            response
+        ) {
+            if (!error) {
+                let res = null;
+                for (let i = 0; i < tweets.length; i++) {
+                    let e = tweets[i];
+                    if (!e.retweeted_status) {
+                        let match = e.text.match(/[0-9]+\.[0-9]+\,[0-9]+/g);
+                        if (match) {
+                            res = {
+                                img: false,
+                                data: e.text
+                            };
+                        }
+                        break;
+                    }
+                }
+                if (!res) {
+                    for (let i = 0; i < tweets.length; i++) {
+                        let e = tweets[i];
+                        if (
+                            !e.retweeted_status &&
+                            e.entities.media &&
+                            e.entities.media.length === 1 &&
+                            e.entities.media[0].media_url_https
+                        ) {
+                            let url = e.entities.media[0].media_url_https;
+                            try {
+                                const data = await ocrSpaceApi.parseImageFromUrl(
+                                    url,
+                                    options
+                                );
+                                let match = data.parsedText.match(
+                                    /PROMEDIO Bs.S/gm
+                                );
+                                if (match) {
+                                    res = {
+                                        img: true,
+                                        data: url
+                                    };
+                                    break;
+                                }
+                            } catch (e) {
+                                console.log(e);
+                            }
+                        }
+                    }
+                }
+                resolve(res);
+            } else {
+                console.log(error);
+            }
+        });
+    });
+
+module.exports = {
+    getAirtmRates,
+    getDolarToday,
+    getMonitor
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { getAirtmRates, getDolarToday, getMonitor } = require('./data');
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/airtm', async (req, res) => {
+    const data = await getAirtmRates();
+    return res.json(data);
+});
+
+app.get('/airtm/sell', async (req, res) => {
+    const data = await getAirtmRates();
+    return res.json(data.sell);
+});
+
+app.get('/airtm/general', async (req, res) => {
+    const data = await getAirtmRates();
+    return res.json(data.general);
+});
+
+app.get('/airtm/buy', async (req, res) => {
+    const data = await getAirtmRates();
+    return res.json(data.buy);
+});
+
+app.get('/dolartoday', async (req, res) => {
+    const data = await getDolarToday();
+    return res.json(data);
+});
+
+app.get('/monitor', async (req, res) => {
+    const data = await getMonitor();
+    return res.json(data);
+});
+
+app.listen(process.env.PORT, '0.0.0.0', function() {
+    console.log('Web Server listening on port ' + process.env.PORT);
+});
