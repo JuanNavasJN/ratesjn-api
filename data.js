c67ddcbac7d9ca8105240db2924a0d4b65decfa7
Juan Navas <navascruzf@gmail.com>
2020-05-23 12:06:03 -0400

    update

--- data.js
+++ data.js
@@ -8,10 +8,10 @@ const dolarToday = 'https://s3.amazonaws.com/dolartoday/data.json';
 const ocrSpaceApi = require('ocr-space-api');
 
 const options = {
-    apikey: '78f7cf9a7088957',
+    apikey: process.env.OCR_APIKEY,
     language: 'spa',
     imageFormat: 'image/jpg',
-    isOverlayRequired: true
+    isOverlayRequired: true,
 };
 
 const getAirtmRates = _ =>
@@ -24,7 +24,7 @@ const getAirtmRates = _ =>
             const data = {
                 buy,
                 general,
-                sell
+                sell,
             };
 
             resolve(data);
@@ -60,11 +60,11 @@ const getMonitor = _ =>
             consumer_key: process.env.CONSUMER_KEY,
             consumer_secret: process.env.CONSUMER_SECRET,
             access_token_key: process.env.ACCESS_TOKEN_KEY,
-            access_token_secret: process.env.ACCESS_TOKEN_SECRET
+            access_token_secret: process.env.ACCESS_TOKEN_SECRET,
         });
 
         const params = { screen_name: 'monitordolarvzl' };
-        client.get('statuses/user_timeline', params, async function(
+        client.get('statuses/user_timeline', params, async function (
             error,
             tweets,
             response
@@ -74,21 +74,17 @@ const getMonitor = _ =>
                 for (let i = 0; i < tweets.length; i++) {
                     let e = tweets[i];
                     if (!e.retweeted_status) {
-                        let match = e.text.match(/[0-9]+\.[0-9]+\,[0-9]+/g);
+                        let match = e.text.match(/[0-9]+.[0-9]+[,|.]+[0-9]+/g);
                         if (match) {
                             res = {
                                 img: false,
-                                data: e.text
+                                data: e.text,
+                                value: match[0],
                             };
                             break;
                         }
-                    }
-                }
-                if (!res) {
-                    for (let i = 0; i < tweets.length; i++) {
-                        let e = tweets[i];
+
                         if (
-                            !e.retweeted_status &&
                             e.entities.media &&
                             e.entities.media.length === 1 &&
                             e.entities.media[0].media_url_https
@@ -102,10 +98,16 @@ const getMonitor = _ =>
                                 let match = data.parsedText.match(
                                     /PROMEDIO Bs.S/gm
                                 );
+
                                 if (match) {
+                                    let match2 = data.parsedText.match(
+                                        /[0-9]+.[0-9]+[,|.]+[0-9]+/g
+                                    );
+
                                     res = {
                                         img: true,
-                                        data: url
+                                        data: url,
+                                        value: match2[0],
                                     };
                                     break;
                                 }
@@ -125,5 +127,5 @@ const getMonitor = _ =>
 module.exports = {
     getAirtmRates,
     getDolarToday,
-    getMonitor
+    getMonitor,
 };
